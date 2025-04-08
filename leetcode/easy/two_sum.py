db7b8421d67384034ce6918b0b88eaa719089dab
MauriceWa <144155257+MauriceWa@users.noreply.github.com>
2025-04-08 12:10:59 +0200

    Create two_sum.py
    Begonnen met werk aan de two_sum oplossing, momenteel niet functioneel zoals nodig is.

---
+++ leetcode/easy/two_sum.py
@@ -0,0 +1,12 @@
+def twoSum():
+    nums = [8, 7, 11, 15]
+    target = 15
+    for num in nums:
+        comparison = nums[0] + nums[1]
+        if comparison == target:
+            return print(comparison)
+        else:
+            comparison
+
+
+twoSum()
