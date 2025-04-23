5ce0beab3e7e17b5de8c6c11ceab3ba818d58051
MauriceWa <144155257+MauriceWa@users.noreply.github.com>
2025-04-23 18:14:53 +0200

    Update two_sum.py

--- leetcode/easy/two_sum.py
+++ leetcode/easy/two_sum.py
@@ -9,4 +9,27 @@ def twoSum():
             comparison
 
 
-twoSum()
+
+
+
+def tweeSum():
+    nums = [8, 7, 11, 15]
+    target = 15
+    for index1, num in enumerate(nums):
+        for index2 in range(index1 + 1, len(nums)):
+            comparison = num + nums[index2]
+            if comparison == target:
+                return print(comparison)
+
+tweeSum()
+
+
+def driesum():
+    hashmap = {}
+
+    for i, n in enumerate(nums):
+        diff = target - n
+        if diff in hashmap:
+            return [hashmap[diff], i]
+        hashmap[n] = i
+        return
